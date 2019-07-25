54937b272890d92dd09c195a6946ef9bdddde66f
VermouthX <vermouth7dante@gmail.com>
2019-07-25 15:50:39 +0800

    refine scripts booting

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -4,11 +4,10 @@ import markdown
 SOURCE_FILE = os.path.join(os.path.dirname(__file__), '..', 'CHANGELOG.md')
 DEST_PATH = os.path.join(os.path.dirname(__file__), '..', 'build')
 
-if __name__ == '__main__':
-  if not os.path.exists(DEST_PATH):
+if not os.path.exists(DEST_PATH):
     os.makedirs(DEST_PATH)
-  with open(SOURCE_FILE, 'r') as source:
+with open(SOURCE_FILE, 'r') as source:
     html = markdown.markdown(source.read())
     html = html.replace('<h1>Changelog</h1>\n', '')
-  with open(os.path.join(DEST_PATH, 'CHANGELOG.html'), 'w') as output_file:
+with open(os.path.join(DEST_PATH, 'CHANGELOG.html'), 'w') as output_file:
     output_file.write(html)
--- scripts/readme.py
+++ scripts/readme.py
@@ -5,31 +5,30 @@ from bs4 import BeautifulSoup
 SOURCE_FILE = os.path.join(os.path.dirname(__file__), '..', 'README.md')
 DEST_PATH = os.path.join(os.path.dirname(__file__), '..', 'build')
 
-if __name__ == '__main__':
-    if not os.path.exists(DEST_PATH):
-        os.makedirs(DEST_PATH)
+if not os.path.exists(DEST_PATH):
+    os.makedirs(DEST_PATH)
 
-    with open(SOURCE_FILE, 'r') as source:
-        html = markdown.markdown(source.read())
-        soup = BeautifulSoup(html, 'html.parser')
+with open(SOURCE_FILE, 'r') as source:
+    html = markdown.markdown(source.read())
+    soup = BeautifulSoup(html, 'html.parser')
 
-        # Remove title
-        soup.find('h1').decompose()
-        # Remove build badges
-        soup.find('p').decompose()
+    # Remove title
+    soup.find('h1').decompose()
+    # Remove build badges
+    soup.find('p').decompose()
 
-        # Set image widths
-        for img in soup.find_all('img'):
-            img['width'] = '700'
+    # Set image widths
+    for img in soup.find_all('img'):
+        img['width'] = '700'
 
-        # Add margin above images
-        for img in soup.find_all('img'):
-            img.insert_before(soup.new_tag('br'))
+    # Add margin above images
+    for img in soup.find_all('img'):
+        img.insert_before(soup.new_tag('br'))
 
-        # remove installation
-        installation = soup.find('h2', text='Installation')
-        installation.find_next('ol').decompose()
-        installation.decompose()
+    # remove installation
+    installation = soup.find('h2', text='Installation')
+    installation.find_next('ol').decompose()
+    installation.decompose()
 
-    with open(os.path.join(DEST_PATH, 'README.html'), 'w') as output_file:
-        output_file.write(str(soup).strip())
+with open(os.path.join(DEST_PATH, 'README.html'), 'w') as output_file:
+    output_file.write(str(soup).strip())
