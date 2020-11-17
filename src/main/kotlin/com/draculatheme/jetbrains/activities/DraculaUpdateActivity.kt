4f8eadcaf902342dbd198b35c685b47c72dc2ee0
VermouthX <vermouth7dante@gmail.com>
2020-11-17 21:57:37 +0800

    :art: Refine codes

--- src/main/kotlin/com/draculatheme/jetbrains/activities/DraculaUpdateActivity.kt
+++ src/main/kotlin/com/draculatheme/jetbrains/activities/DraculaUpdateActivity.kt
@@ -3,10 +3,11 @@ package com.draculatheme.jetbrains.activities
 import com.draculatheme.jetbrains.DraculaMeta
 import com.draculatheme.jetbrains.notifications.DraculaNotifications
 import com.draculatheme.jetbrains.settings.DraculaSettings
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
-class DraculaUpdateActivity : StartupActivity {
+class DraculaUpdateActivity : StartupActivity, DumbAware {
     override fun runActivity(project: Project) {
         val settings = DraculaSettings.instance
         val updated = DraculaMeta.currentVersion != settings.version
