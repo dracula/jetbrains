6ea21a98df983fed3a8f8a50489f05c4d5abc65f
VermouthX <vermouth7dante@gmail.com>
2021-01-23 16:15:46 +0800

    ♻️ Notification codes refactor

--- src/main/kotlin/com/draculatheme/jetbrains/notifications/DraculaNotifications.kt
+++ src/main/kotlin/com/draculatheme/jetbrains/notifications/DraculaNotifications.kt
@@ -6,22 +6,39 @@ import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IconLoader
+import org.intellij.lang.annotations.Language
 
 object DraculaNotifications {
     private const val notificationGroupID: String = "dracula-theme"
 
-    private val releaseNote = """
-        What's New?<br>
+    @Language("HTML")
+    private val whatsNew = """
         <ul>
             <li>Enhanced File background color</li>
             <li>Revised editor popup background</li>
             <li>Enhanced Dracula editor caret background and selection background</li>
             <li>Regressed Italic function parameters and decorators</li>
         </ul>
-        Please visit the <a href="https://github.com/dracula/jetbrains/blob/master/CHANGELOG.md">Changelog</a> for more details.<br>
-        For premium package, check out <a href="https://gumroad.com/a/477820019">Dracula PRO</a>.<br>
-        Enjoy this theme? Consider <a href="https://github.com/dracula/jetbrains">STAR</a> this project.<br>
-        Thank you for choosing Dracula.
+    """.trimIndent()
+
+    @Language("HTML")
+    private val releaseNote = """
+        <div>
+            <h3>What's New</h3>
+            $whatsNew
+            <p>Please visit the <a href="https://github.com/dracula/jetbrains/blob/master/CHANGELOG.md">Changelog</a> for more details.</p>
+            <p>For premium package, check out <a href="https://gumroad.com/a/477820019">Dracula PRO</a>.</p>
+            <p>Enjoy this theme? Consider <a href="https://github.com/dracula/jetbrains">STAR</a> this project.</p>
+            <p>Thank you for choosing Dracula.</p>
+        </div>
+    """.trimIndent()
+
+    @Language("HTML")
+    private val welcomeMessage = """
+        <div>
+            <p>Thank you for choosing Dracula.</p>
+            <p>Dracula Theme is fully open-source. If you enjoy this theme, consider <a href="https://github.com/dracula/jetbrains">STAR</a> this project.</p>
+        </div>
     """.trimIndent()
 
     @JvmField
@@ -43,10 +60,7 @@ object DraculaNotifications {
         NotificationGroupManager.getInstance().getNotificationGroup(notificationGroupID)
             .createNotification(
                 title = "Dracula Theme v${DraculaMeta.currentVersion} is installed",
-                content = """
-                    Dracula Theme is fully open-source. If this theme is helpful to you, consider <a href="https://github.com/dracula/jetbrains">STAR</a> this project. 
-                    Thank you for choosing Dracula.
-                """.trimIndent(),
+                content = welcomeMessage,
                 type = NotificationType.INFORMATION,
                 listener = NotificationListener.URL_OPENING_LISTENER
             )
