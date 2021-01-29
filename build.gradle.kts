47fecff39f2d75743201f388f0f7b39ccad37767
VermouthX <vermouth7dante@gmail.com>
2021-01-29 10:16:36 +0800

    🐛 Fix compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,8 @@ tasks {
         enabled = false
     }
     patchPluginXml {
+        sinceBuild("203.3645.34")
+        untilBuild("211.*")
         val changelogFile = file("${project.buildDir}/CHANGELOG.html")
         val readmeFile = file("${project.buildDir}/README.html")
         if (changelogFile.exists()) {
