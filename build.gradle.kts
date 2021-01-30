715ba8ddf3a7c20456050f79ccdd5bd4ee05eb71
VermouthX <vermouth7dante@gmail.com>
2021-01-30 10:34:48 +0800

    ⬆️ Build with JDK 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,12 @@ intellij {
 }
 
 tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
+    sourceCompatibility = "11"
+    targetCompatibility = "11"
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 tasks {
