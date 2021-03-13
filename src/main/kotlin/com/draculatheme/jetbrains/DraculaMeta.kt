7b545b50a20a7270492eca44c3379587b0fbdb43
VermouthX <vermouth7dante@gmail.com>
2021-03-13 15:18:52 +0800

    ⬆️ Build with JDK 11 and 2020.1

--- src/main/kotlin/com/draculatheme/jetbrains/DraculaMeta.kt
+++ src/main/kotlin/com/draculatheme/jetbrains/DraculaMeta.kt
@@ -1,12 +1,9 @@
 package com.draculatheme.jetbrains
 
-import com.intellij.ide.plugins.PluginManager
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.extensions.PluginId
 
 object DraculaMeta {
-    private const val PLUGIN_ID = "com.vermouthx.idea"
-
     val currentVersion: String
-        get() = PluginManager.getPlugin(PluginId.getId(PLUGIN_ID))?.version ?: ""
-
+        get() = PluginManagerCore.getPlugin(PluginId.getId("com.vermouthx.idea"))?.version ?: ""
 }
