36c7f94731a3a9740fe2830ce4d815a05318be86
Zihan Ma <zihan.ma@protonmail.com>
2022-10-13 22:30:52 +0800

    🚨 Setting class warning workaround

--- src/main/kotlin/com/draculatheme/jetbrains/settings/DraculaSettings.kt
+++ src/main/kotlin/com/draculatheme/jetbrains/settings/DraculaSettings.kt
@@ -1,15 +1,15 @@
 package com.draculatheme.jetbrains.settings
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
-import com.intellij.openapi.components.service
 
 @State(name = "DraculaSetting", storages = [Storage("dracula-theme.xml")])
 class DraculaSettings : PersistentStateComponent<DraculaState> {
     companion object {
         val instance: DraculaSettings
-            get() = service()
+            get() = ApplicationManager.getApplication().getService(DraculaSettings::class.java)
     }
 
     private var myState = DraculaState()
