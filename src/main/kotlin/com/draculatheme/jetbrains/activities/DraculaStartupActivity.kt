9f7bc08b861b8128692de020f90e16c031d0cf5d
Zihan Ma <zihan.ma@protonmail.com>
2024-04-14 01:05:11 +0800

    🐛 Fix compatibility issue

--- src/main/kotlin/com/draculatheme/jetbrains/activities/DraculaStartupActivity.kt
+++ src/main/kotlin/com/draculatheme/jetbrains/activities/DraculaStartupActivity.kt
@@ -5,10 +5,10 @@ import com.draculatheme.jetbrains.notifications.DraculaNotification
 import com.draculatheme.jetbrains.settings.DraculaSettings
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.startup.ProjectActivity
 
-class DraculaStartupActivity : StartupActivity, DumbAware {
-    override fun runActivity(project: Project) {
+class DraculaStartupActivity : ProjectActivity, DumbAware {
+    override suspend fun execute(project: Project) {
         val settings = DraculaSettings.instance
         if (settings.version.isEmpty()) {
             settings.version = DraculaMeta.currentVersion
--- src/main/kotlin/com/draculatheme/jetbrains/listeners/DraculaThemeChangeListener.kt
+++ src/main/kotlin/com/draculatheme/jetbrains/listeners/DraculaThemeChangeListener.kt
@@ -9,13 +9,13 @@ class DraculaThemeChangeListener : LafManagerListener {
 
     private val editorColorsManager = EditorColorsManager.getInstance()
 
-    private var previousUI = LafManager.getInstance().currentLookAndFeel.name
+    private var previousUI = LafManager.getInstance().currentUIThemeLookAndFeel.name
 
     override fun lookAndFeelChanged(lafManager: LafManager) {
-        val currentUI = lafManager.currentLookAndFeel.name
+        val currentUI = lafManager.currentUIThemeLookAndFeel.name
         if (previousUI != currentUI) {
             if (currentUI == DraculaVariant.Dracula.label || currentUI == DraculaVariant.DraculaColorful.label) {
-                editorColorsManager.globalScheme = editorColorsManager.getScheme("_@user_$currentUI")
+                editorColorsManager.setGlobalScheme(editorColorsManager.getScheme("_@user_$currentUI"))
             }
         }
         previousUI = currentUI
